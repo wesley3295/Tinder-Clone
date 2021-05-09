11a14337e949277eadb3ded6e349b068d773310f
Wesley <wspencer3295@yahoo.com>
2021-05-08 19:08:34 -0700

    import components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
 import Header from './Header'
+import TinderCards from './TinderCards'
+import SwipeButtons from './SwipeButtons'
 function App() {
   return (
     <div className="app">
      
     <Header/>
-    {/* TinderCards */}
-    {/* SwipeButtons*/}
+    <TinderCards/>
+    <SwipeButtons/>
     
-
-
-
     </div>
   );
 }
