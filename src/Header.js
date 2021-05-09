d6428ac0bdd1c47e1b8d33d845c096fdea90c4a9
Wesley <wspencer3295@yahoo.com>
2021-05-08 19:08:02 -0700

    +icons btns & css

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./Header.css"
+import PersonIcon from "@material-ui/icons/Person"
+import ForumIcon from "@material-ui/icons/Forum"
+import IconButton from "@material-ui/core/IconButton"
+
 const Header = () => {
     return (
         <div className="header">
             
+            <IconButton>
+                <PersonIcon fontSize="large" classNAme="header__icon" />
+            </IconButton>
+
+            <IconButton>
+                <img className="header__logo" src="https://styles.redditmedia.com/t5_2w7mz/styles/communityIcon_jmyuhs81jl211.png?width=256&s=a23d21debdfe842e1b4d9ce3c24f3ab8be5d0e18" alt="Tinder Logo"/>
+            </IconButton>
+
+            <IconButton>
+                <ForumIcon fontSize="large" classNAme="header__icon" />
+            </IconButton>
+
         </div>
     )
 }
